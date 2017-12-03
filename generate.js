9fd330214788da26229ca1aa920631a1ee199f82
Tyler Stewart <tylers490@gmail.com>
2017-12-02 21:06:32 -0700

    feat: output various image sizes
    Modifies the page's viewport to generate images of various sizes.
    Uses `$eval` to change the underlining css on the fly.
    Changes font to Overpass Mono

--- generate.js
+++ generate.js
@@ -1,5 +1,17 @@
 const puppeteer = require('puppeteer');
 
+async function screenshot(page, width, height) {
+  console.log(`Generating ${width}x${height}...`);
+  return await page.setViewport({
+    width,
+    height
+  })
+  .then(() => page.screenshot({
+    path: `logo_${width}x${height}.png`,
+    omitBackground: true
+  }));
+}
+
 (async () => {
   console.log('Preparing...');
 
@@ -8,22 +20,19 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
   await page.goto(logoPath);
 
-  try {
-    console.log('Generating logo...');
-    await page.screenshot({
-      path: 'logo.png',
-      omitBackground: true,
-      clip: {
-        x: 0,
-        y: 0,
-        width: 200,
-        height: 200
-      }
-    });
-  } catch (err) {
-    console.error(err);
-    process.exit(2)
-  }
+  await screenshot(page, 200, 200);
+
+  await page.$eval('h1', h1 => h1.style['font-size'] = '60px');  
+
+  await screenshot(page, 960, 150);
+
+  await page.$eval('body', h1 => h1.style['justify-content'] = 'flex-end');
+  await page.$eval('h1', h1 => {
+    h1.style['justify-content'] = 'flex-end';
+    h1.style['padding'] = '40px';
+  });
+
+  await screenshot(page, 600, 338);
 
   await browser.close();
 })();
